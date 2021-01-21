ab4f26221e2c43ffc2442db8eff5e564739d3ea6
gazibur <gaziburrahman1999@gmail.com.com>
2021-01-21 14:14:27 +0530

    remove command completed

--- notes-app/app1.js
+++ notes-app/app1.js
@@ -21,4 +21,24 @@ const notefile=require("./notes1.js")
       notefile.addnotes(argv.title,argv.body)
     }
   })
+  
+  yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    builder: {
+      title: {
+        title: 'remove',
+        demandOption: true,
+        type: String
+      },
+      body: {
+        title: 'removed note',
+        demandOption: true,
+        type: String
+      }
+    },
+    handler() {
+      notefile.removenotes(argv.title)
+    }
+  })
   yargs.parse()
--- notes-app/notes1.js
+++ notes-app/notes1.js
@@ -1,29 +1,19 @@
 const fs = require("fs")
-const loadnotes = () => {
-  try {
-    const buffer = fs.readFileSync('./text.json')
-    const json = buffer.toString()
-let notesarr=JSON.parse(json)
-return notesarr
-  } catch {
-    return []
-  }
-}
+
+
 addnotes = (title, body) => {
   let notesarr = loadnotes()
-
   if (notesarr.length === 0) {
     notesarr.push({'title': title,'body': body},)
     let json = JSON.stringify(notesarr)
     savenotes(json)
     console.log('new note added')
   } else if (notesarr.length !== 0) {
-    console.log('it works ');
     const duplicates = notesarr.find((note) => note.title === title)
     if (!duplicates) {
       notesarr.push({'title': title,'body': body})
       const json = JSON.stringify(notesarr)
-
+      console.log(json)
       savenotes(json)
       console.log('new note added')
     } else if (duplicates) {
@@ -31,11 +21,38 @@ addnotes = (title, body) => {
     }
   }
 }
+
+
+const removenotes=(title)=>{
+ const notes=loadnotes()
+ const restItem= notes.filter((note)=>note.title!==title)
+
+ if(restItem.length<notes.length){
+const stringify=JSON.stringify(restItem)
+savenotes(stringify)
+console.log('Note Removed')
+}else{
+  console.log('Note Not found')
+}
+}
+const loadnotes = () => {
+  try {
+    const buffer = fs.readFileSync('./text.json')
+    const json = buffer.toString()
+    let notesarr = JSON.parse(json)
+    return notesarr
+  } catch {
+    return []
+  }
+}
+
 const savenotes = (notes) => {
-  let json = JSON.stringify(notes)
+ // let json = JSON.stringify(notes)
   //console.log(notesarr)
   fs.writeFileSync("./text.json", notes)
 }
+
 module.exports = {
   addnotes: addnotes,
+  removenotes:removenotes
 };
